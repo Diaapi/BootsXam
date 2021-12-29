4db11f75de6437b6f477c9462ee29862fe246f7d
Diego Aleman <alemandiego36@gmail.com>
2021-12-29 12:24:02 -0400

    Add project files.

---
+++ bootsxam/Buttons/ButtonPrimary.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bootsxam
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Button : ContentView
+    {
+        public BindableProperty ButtonTextProperty = BindableProperty.Create(nameof(ButtonText),typeof(string),typeof(Button),defaultValue: string.Empty,defaultBindingMode: BindingMode.OneWay, propertyChanged: TitleTextProperty);
+
+        private static void TitleTextProperty(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (Button)bindable;
+            control.btn.Text = newValue?.ToString();
+        }
+
+        public string ButtonText {
+            get 
+            {
+                return base.GetValue(ButtonTextProperty).ToString();
+            }
+            set 
+            { 
+            base.SetValue(ButtonTextProperty, value);
+            } 
+        }
+        public Button()
+        {
+            InitializeComponent();
+        }
+    }
+}
